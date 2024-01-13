515f7e83687ca16b09383dea3555b5ff23c74fb2
Diego <digule@gmail.com>
2024-01-13 12:19:20 +0100

    Commit 2

--- Tecla.java
+++ Tecla.java
@@ -2,8 +2,17 @@ class Tecla {
     private boolean presionada;
     private String caracter;
     
-    public void Tecla(String selectedKey) {
+    public Tecla(String selectedKey) {
         caracter = selectedKey;
+        presionada = false;
+    }
+    
+    public void presionar(){
+        presionada = true;
+    }
+    
+    public void cambiarCaracter(String changeSelectedKey){
+        caracter = changeSelectedKey;
         presionada = false;
     }
 }
